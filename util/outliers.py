89b72be38fa75e09af5e426e8b5ebae6c1e2cad7
Casey Weed <me@caseyweed.net>
2015-10-13 19:12:31 -0400

    now does the drop right (tm), with included Ri column, will keep locuses regardless, but will only check for locus outliers with --locuses flag

--- util/outliers.py
+++ util/outliers.py
@@ -20,15 +20,19 @@ import os
 
 attrs = namedtuple('attrs', 'std, avg')
 
-def get_outliers(data, attrs):
+def get_outliers(data, attrs, keep_locuses=False):
+    temp = data
+    if not keep_locuses:
+        locuses = temp.filter(like='Locus').columns
+        temp = temp.drop(locuses, axis=1)
     outliers = defaultdict(list)
     for column, val in attrs.items():
         l = 2. * val.std - val.avg
         h = 2. * val.std + val.avg
-        for idx in xrange(0, len(data)):
-            x = data.iloc[idx][column]
+        for idx in xrange(0, len(temp)):
+            x = temp.iloc[idx][column]
             if not(l <= x <= h):
-                label = data.iloc[idx]['Label']
+                label = temp.iloc[idx]['Label']
                 outliers[column].append(label)
     return outliers
 
@@ -53,49 +57,31 @@ def flatten(*args):
         else:
             yield x
 
-def do_drop(data, outliers, filename='dropped.csv'):
-    # Create Ri column by removing 'ARC' from Label and parsing as int
-    starting = len(data)
-    ri = data['Label'].apply(lambda x: int(x[3:]))
-    data.insert(0, 'Ri', ri)
-    data.sort_values(['Ri'], inplace=True)
-    # Create set from all outliers to avoid dropping already removed labels
+def do_drop_right(data, outliers, filename='dropped.csv'):
     flattened_outliers = list(flatten(outliers.values()))
     outliers_set = set(sorted(flattened_outliers))
-    common_outliers = len(flattened_outliers) - len(outliers_set)
-    # For each outlier in the set, find its location, drop it in place,
-    # then decrement the values for its position and above.
-    # Also, WHY THE FUCK DOES REVERSING THE LIST FIX ALL THE ISSUES?
-    for o in reversed(list(outliers_set)):
+    if 'Unnamed: 0' in data.columns:
+        data.drop(data[['Unnamed: 0']], axis=1, inplace=True)
+    for o in outliers_set:
         r = data.loc[data['Label'] == o]
         ridx = r.index
         data.drop(data.index[ridx], inplace=True)
         data.reset_index(inplace=True, drop=True)
-        data.ix[data.index >= ridx.item(), 'Ri'] -= 1
-        print 'Removed {} (@ {})'.format(o, ridx.item())
-    # Reindex and save to file
-    data.sort_index(inplace=True)
-    data.to_csv(filename)
-    print 'From {} rows to {}'.format(starting, len(data))
-    print 'Removed a total of {} rows'.format(len(outliers_set))
-    print '{} common outliers'.format(common_outliers)
+    data.to_csv(filename, index=False)
 
 def main(filename, save, drop=False, keep_locuses=False):
     data = pd.read_csv(filename)
-    locuses = data.filter(like='Locus').columns
-    # Drop locuses if not needed
-    if not keep_locuses:
-        data.drop(locuses, axis=1, inplace=True)
-    # Collect attributes for each column with numeric data
     column_attrs = dict()
-    numeric_columns = data._get_numeric_data().columns
+    if not keep_locuses:
+        lcs = data.filter(like='Locus').columns
+        numeric_columns = data.drop(lcs, axis=1)._get_numeric_data().columns
+    else:
+        numeric_columns = data._get_numeric_data().columns
     for col in numeric_columns:
         column_attrs[col] = attrs(std=data[col].std(), avg=data[col].mean())
-    # Collect outliers
-    outliers = get_outliers(data, column_attrs)
-    # Save or echo results
+    outliers = get_outliers(data, column_attrs, keep_locuses)
     if save:
-        save_outliers(outliers, data._get_numeric_data(), column_attrs, save)
+        save_outliers(outliers, outliers.keys(), column_attrs, save)
     else:
         for column in data._get_numeric_data():
             labels = ', '.join(outliers[column])
@@ -104,7 +90,7 @@ def main(filename, save, drop=False, keep_locuses=False):
         if common:
             print('Common rows: {}'.format(', '.join(common)))
     if drop:
-        do_drop(data, outliers, drop)
+        do_drop_right(data, outliers, drop)
 
 if __name__ == '__main__':
     args = docopt(__doc__)
